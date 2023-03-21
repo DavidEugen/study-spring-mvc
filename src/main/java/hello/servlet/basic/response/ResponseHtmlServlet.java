1835e31bb8097d9ce8c42c16e481d6d597698018
DavidEugen-Mac <platonicojju@gmail.com>
2023-03-21 10:46:45 +0900

    Feat: response 기능들
    기본
    response.setHeader()으로 처리
    
    content 편의 기능
    response.setContentType("text/plain");
    response.setCharacterEncoding("utf-8");
    response.setContentLength(2);
    
    쿠키 편의 메서드
    Cookie cookie = new Cookie("myCookie", "good");
    cookie.setMaxAge(600); //600초
    response.addCookie(cookie);
    
    redirect 편의 메서드
    status 302 code + header("Location", uri )
    => response.sendRedirect(uri)

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,77 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-header")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK); //200
+
+        //[response-headers]
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // 캐쉬 지우기
+        response.setHeader("Pragma", "no-cache");
+//        response.setHeader("my-header", "hello"); //custom cookie
+
+        content(response);
+        cookie(response);
+
+        redirect(response);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+    }
+
+    //content 편의 metod
+    private void content(HttpServletResponse response) {
+
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8"); //이렇게 쓸 수 있으나
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        //response.setContentLength(2); //(생략시 자동 생성)
+        //writer.println("ok")시  3이 나오는 이유는 println으로 ln 개행이 들어갔을므로...
+    }
+
+    //쿠키 편의 메서드
+    private void cookie(HttpServletResponse response) {
+
+        //Set-Cookie: myCookie=good; Max-Age=600;
+
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600"); //이렇게 쓸 수 있으나
+
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+
+        response.addCookie(cookie); //Cookie를 마지막에 response에 넣어 준다.
+
+    }
+
+    //redirect 편의 메서드
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html"); // 이렇게 사용할 수 있으나
+
+        response.sendRedirect("/basic/hello-form.html");
+    }
+
+
+}
